31b991d7feb6facbfefb617833496a4c0051a5c4
Jorge Luis Bou-saad <jbousing@gmail.com>
2021-03-11 01:45:03 -0400

    creando queries con parametros en graphql

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-const { graphql, buildSchema } = require("graphql");
+//const { graphql, buildSchema } = require("graphql");
+const { makeExecutableSchema } = require("graphql-tools");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const app = express();
@@ -7,27 +8,29 @@ const port = process.env.port || 3000;
 const { readFileSync} = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8');
 //definiendo el schema
 
-const schema = buildSchema(
-    readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
-  );
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
 
 
 //EJECUTAR QUERY
-graphql(schema,'{getCourses {_id title }}', resolvers).then(data => {
+/*graphql(schema,'{getCourses {_id title }}', resolvers).then(data => {
     data.data.getCourses.forEach(element => { 
         console.log("test");
         console.log(element.title);
     })
-});
+}); */
 
 app.use(
     '/graphql',
     graphqlHTTP({
       schema: schema,
       graphiql: true,
-      rootValue: resolvers
+      //rootValue: resolvers
     }),
   );
 
